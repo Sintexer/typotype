01aa6a7c2f0934c3862486c19ad13bec32f7d09a
Ilya Buglakov <neonlightnight@gmail.com>
2022-04-30 00:01:24 +0300

    initial-commit

---
+++ src/main/kotlin/com/github/sintexer/Application.kt
@@ -0,0 +1,13 @@
+package com.github.sintexer
+
+import com.github.sintexer.plugins.configureRouting
+import com.github.sintexer.plugins.configureSerialization
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
